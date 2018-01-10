bb4551b0f722e3aabe89461b71cffdfde86b2bfa
nttr <nttr@BI-23560.local>
2018-01-10 16:39:58 +0900

    second commit

--- app/src/main/java/com/example/nttr/quiz/MainActivity.java
+++ app/src/main/java/com/example/nttr/quiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.nttr.quiz;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -36,6 +37,21 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     ImageView imageView;
 
+    boolean kaitou;
+
+    Hero hero;
+
+    int countScore = 0;
+
+    int score = 0;
+
+    int nowNumber = -1;
+
+    MediaPlayer maru;
+    MediaPlayer batu;
+
+    final int maxQuestionNumber = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,17 +77,28 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         imageView = (ImageView) findViewById(R.id.imageRed);
 
+        countScore = 0;
+        score = 0;
+
+        maru = MediaPlayer.create(getApplicationContext(),R.raw.maru);
+        batu = MediaPlayer.create(getApplicationContext(),R.raw.batu);
+
         // Quizを1から始めるのでリセットする
         resetQuiz();
     }
 
     // ③ 出題するクイズのリストを作成します
     void createQuizList() {
-        Quiz quiz1 = new Quiz("ドラえもんの身長は何センチ？", "127.3cm", "128.3cm", "129.3cm", "129.3cm");
-        Quiz quiz2 = new Quiz("ドラえもんの嫌いな生き物は？", "猫", "タヌキ", "ネズミ", "ネズミ");
-        Quiz quiz3 = new Quiz("ドラえもんの好きな食べ物は？", "ラーメン", "牛丼", "どら焼き", "どら焼き");
-        Quiz quiz4 = new Quiz("ドラえもんは昔何色だった？", "黄色", "赤色", "水色", "黄色");
-        Quiz quiz5 = new Quiz("アニメ版ドラえもん、「ドラえもんのうた」に出てこない道具は？", "タケコプター", "スモールライト", "どこでもドア", "スモールライト");
+        Quiz quiz1 = new Quiz("日本で一番高い山は富士山ですが、世界で一番高い山は？", "キリマンジャロ", "エベレスト", "K2", "エベレスト");
+        Quiz quiz2 = new Quiz("元素記号Coで表されるものはコバルトですが、化学式COで表されるものは？", "一酸化炭素", "コロンビア", "クーリングオフ", "一酸化炭素");
+        Quiz quiz3 = new Quiz("重さの単位でmgはミリグラムと読みますが、Mgは何と読むでしょう？", "マグネシウム", "メタグラム", "メガグラム", "メガグラム");
+        Quiz quiz4 = new Quiz("日本で一番大きい湖は琵琶湖ですが、世界で一番広い湖は？", "カスピ海", "ビクトリア湖", "太平洋", "カスピ海");
+        Quiz quiz5 = new Quiz("世界で一番高いビルはブルジュ・ハリファですが、日本一高いビルは？", "あべのハルカス", "横浜ランドマークタワー", "東京スカイツリー", "あべのハルカス");
+        Quiz quiz6 = new Quiz("赤外線の略称はIRですが、紫外線の略称は？", "UV", "AR", "DD", "UV");
+        Quiz quiz7 = new Quiz("rightは日本語で右という意味ですが、lightは日本語で何という意味？", "センター", "光", "左", "光");
+        Quiz quiz8 = new Quiz("アイスランドの首都はレイキャヴィークですが、アイルランドの首都は？", "リムリック", "コーク", "ダブリン", "ダブリン");
+        Quiz quiz9 = new Quiz("ギリシャ文字の最初の文字はαですが、最後の文字は", "プサイ", "カイ", "オメガ", "オメガ");
+        Quiz quiz10 = new Quiz("日本で一番多い苗字は佐藤ですが、アメリカで一番多い名字は？", "マイク", "ブラウン", "スミス", "スミス");
         // Listを実装したArrayListというものを使います
         quizList = new ArrayList<>();
         quizList.add(quiz1);
@@ -79,6 +106,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
         quizList.add(quiz3);
         quizList.add(quiz4);
         quizList.add(quiz5);
+        quizList.add(quiz6);
+        quizList.add(quiz7);
+        quizList.add(quiz8);
+        quizList.add(quiz9);
+        quizList.add(quiz10);
         // Listの中身をシャッフルします
         Collections.shuffle(quizList);
     }
@@ -107,17 +139,20 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     // ⑥ クイズのアップデート
     void updateQuiz() {
+        optionButtons[0].setClickable(true);
+        optionButtons[1].setClickable(true);
+        optionButtons[2].setClickable(true);
+        optionButtons[0].setPressed(false);
+        optionButtons[1].setPressed(false);
+        optionButtons[2].setPressed(false);
         questionNumber++;
-        if (questionNumber < quizList.size()) {
-            if (questionNumber % 2 == 0) {
-                imageView.setImageResource(R.drawable.syakin_rr);
-            } else {
-                imageView.setImageResource(R.drawable.syakin_r);
-            }
+        if (questionNumber < quizList.size() && questionNumber < maxQuestionNumber) {
+            imageView.setImageResource(R.mipmap.syakin_rr2);
             showQuiz();
         } else {
             finish();
             Intent intent = new Intent(this, ResultActivity.class);
+            intent.putExtra("score", score);
             startActivity(intent);
 
 //            // 全ての問題を解いてしまったので、結果を表示します。
@@ -151,48 +186,63 @@ public class MainActivity extends Activity implements View.OnClickListener {
     // ⑦ setOnClickListerを呼び出したViewをクリックした時に呼び出されるメソッド
     @Override
     public void onClick(View view) {
-        // 引数のViewには、押されたViewが入っています。
-        // Buttonが押された時しかよばれないので、キャストといって型を示してあげます。
-        progressClass.cancel(true);
-        Button clickedButton = (Button) view;
-        if (questionNumber < quizList.size()) {
-            Quiz quiz = quizList.get(questionNumber);
-            // ボタンの文字と、答えが同じかチェックします。
-            if (TextUtils.equals(clickedButton.getText(), quiz.answer)) {
-                // 正解の場合だけ1ポイント加算します。
-                point++;
-                Thread nT = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                imageView.setImageResource(R.drawable.maru);
-                                try {
-                                    Thread.sleep(1000);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        });
-                    }
-                });
-                nT.start();
-                try {
-                    nT.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        if(nowNumber != questionNumber){
+            nowNumber = questionNumber;
+            optionButtons[0].setClickable(false);
+            optionButtons[1].setClickable(false);
+            optionButtons[2].setClickable(false);
+            // 引数のViewには、押されたViewが入っています。
+            // Buttonが押された時しかよばれないので、キャストといって型を示してあげます。
+            progressClass.cancel(true);
+            Button clickedButton = (Button) view;
+            if (questionNumber < quizList.size()) {
+                Quiz quiz = quizList.get(questionNumber);
+                // ボタンの文字と、答えが同じかチェックします。
+                if (TextUtils.equals(clickedButton.getText(), quiz.answer)) {
+                    // 正解の場合だけ1ポイント加算します。
+                    kaitou = true;
+                    point++;
+                    score += countScore;
+//                Thread nT = new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        runOnUiThread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                imageView.setImageResource(R.drawable.maru);
+//                                try {
+//                                    Thread.sleep(1000);
+//                                } catch (InterruptedException e) {
+//                                    e.printStackTrace();
+//                                }
+//                            }
+//                        });
+//                    }
+//                });
+//                nT.start();
+//                try {
+//                    nT.join();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
 //                Toast.makeText(this, "正解", Toast.LENGTH_SHORT).show();
-            } else {
+                } else {
+                    kaitou = false;
 //                Toast.makeText(this, "はずれ", Toast.LENGTH_SHORT).show();
+                }
+                // 次の問題にアップデートします。
+//            updateQuiz();
+                hero = new Hero(this);
+                hero.execute();
             }
-            // 次の問題にアップデートします。
-            updateQuiz();
+        }else{
+            Log.d("連打","やめて");
         }
     }
 
     private void startProgress() {
+        progressBar.setProgress(250);
+        countScore = 0;
         progressClass = new ProgressClass(this);
         progressClass.execute();
 
@@ -221,21 +271,28 @@ public class MainActivity extends Activity implements View.OnClickListener {
 //        timer.start();
     }
 
-    public void displayToast() {
-        imageView.setImageResource(R.drawable.batu);
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-
-        }
-//        Toast.makeText(this, "時間切れ", Toast.LENGTH_SHORT).show();
-        updateQuiz();
-    }
+//    public void displayToast() {
+//        imageView.setImageResource(R.drawable.batu);
+//        try {
+//            Thread.sleep(1000);
+//        } catch (Exception e) {
+//
+//        }
+////        Toast.makeText(this, "時間切れ", Toast.LENGTH_SHORT).show();
+//        updateQuiz();
+//    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        progressClass.cancel(true);
+        try{
+            progressClass.cancel(true);
+            hero.cancel(true);
+            maru.stop();
+            batu.stop();
+        }catch (Exception e){
+
+        }
     }
 
     private static class ProgressClass extends AsyncTask<Void, Integer, Boolean> {
@@ -277,15 +334,19 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 while (!isCancelled()) {
                     long nowTime = System.currentTimeMillis();
                     int progressTime = (int) (nowTime - startTime);
-                    mainActivity.progressBar.setProgress(progressTime);
-                    if (progressTime >= c * 300) {
-                        c += 1;
-                        publishProgress(c);
-                    }
+                    mainActivity.countScore = 10000 - progressTime;
 
                     if (progressTime >= 10000) {
                         return true;
                     }
+
+//                    mainActivity.progressBar.setProgress(progressTime);
+                    if (progressTime >= c *250) {
+                        c += 1;
+                        publishProgress(c);
+                    }
+
+
                 }
                 return false;
             } catch (Exception e) {
@@ -302,14 +363,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 String question = mainActivity.quizList.get(mainActivity.questionNumber).content;
                 int len = question.length();
                 for (Integer count : progress) {
+                    mainActivity.progressBar.setProgress(count * 250);
+
+                    if(count % 2 == 0){
+                        mainActivity.imageView.setImageResource(R.mipmap.syakin_rr2);
+                    }else{
+                        mainActivity.imageView.setImageResource(R.mipmap.syakin_r2);
+                    }
+
                     if (len >= count) {
                         String que = question.substring(0, count);
                         mainActivity.contentTextView.setText(que);
-                        if(count % 2 == 0){
-                            mainActivity.imageView.setImageResource(R.drawable.syakin_rr);
-                        }else{
-                            mainActivity.imageView.setImageResource(R.drawable.syakin_r);
-                        }
+
                     }
                 }
             }
@@ -341,99 +406,113 @@ public class MainActivity extends Activity implements View.OnClickListener {
         @Override
         protected void onPostExecute(Boolean result) {
             if (result && !isCancelled()) {
-                mainActivity.displayToast();
+//                mainActivity.displayToast();
+                this.mainActivity.kaitou = false;
+                Hero hero = new Hero(this.mainActivity);
+                hero.execute();
+            }else {
+                cancel(true);
             }
         }
     }
 
-//    private static class Hero extends AsyncTask<Void, Boolean, Boolean> {
+    private static class Hero extends AsyncTask<Void, Boolean, Boolean> {
+
+        private MainActivity mainActivity;
+
+        private Exception exception;
+
+        // AsyncTaskのシングルトンは厳しいらしい
+//    private static ProgressClass progressClass = null;
 //
-//        private MainActivity mainActivity;
-//
-//        private Exception exception;
-//
-//        // AsyncTaskのシングルトンは厳しいらしい
-////    private static ProgressClass progressClass = null;
-////
-////    public static ProgressClass getInstance(MainActivity mainActivity){
-////        if(progressClass == null){
-////            progressClass = new ProgressClass(mainActivity);
-////        }
-////        return progressClass;
-////    }
-//
-//        /**
-//         * コンストラクタ
-//         */
-//        public Hero(MainActivity mainActivity) {
-//            super();
-//            this.mainActivity = mainActivity;
+//    public static ProgressClass getInstance(MainActivity mainActivity){
+//        if(progressClass == null){
+//            progressClass = new ProgressClass(mainActivity);
 //        }
-//
-////        private void setNullActivity(){
-////            this.mainActivity = null;
-////        }
-//
-//        /**
-//         * バックグランドで行う処理
-//         */
-//        @Override
-//        protected Boolean doInBackground(Void... value) {
-//            try {
-//                publishProgress(this.mainActivity.kaitou);
-//                return true;
-//            } catch (Exception e) {
-//                exception = e;
-//                cancel(true);
-//                return false;
-//            }
-//        }
-//
-//        // doInBackground中にUI更新
-//        @Override
-//        protected void onProgressUpdate(Boolean... progress) {
-//            for (Boolean bool : progress) {
-//                try{
-//                    if(bool){
-//                        this.mainActivity.imageView.setImageResource(R.drawable.maru);
-//                    }else{
-//                        this.mainActivity.imageView.setImageResource(R.drawable.maru);
-//                    }
-//                }catch (Exception e){
-//                    cancel(true);
-//                }
-//            }
-//        }
-//
-//        private boolean shouldReject() {
-//            MainActivity activity = this.mainActivity;
-//
-//            if (activity != null) {
-//                return activity.isFinishing() || !activity.hasWindowFocus();
-//            } else {
-//                return true;
-//            }
-//        }
-//
-//        @Override
-//        protected void onCancelled() {
-//            if (!shouldReject()) {
-//                if (exception != null) {
-//                    Log.e("exception", "exception : " + exception);
-//                }
-//            }
-//            exception = null;
-//        }
-//
-//        /**
-//         * バックグランド処理が完了し、UIスレッドに反映する
-//         */
-//        @Override
-//        protected void onPostExecute(Boolean result) {
-//            if (result && !isCancelled()) {
-//
-//            }
-//        }
+//        return progressClass;
 //    }
+
+        /**
+         * コンストラクタ
+         */
+        public Hero(MainActivity mainActivity) {
+            super();
+            this.mainActivity = mainActivity;
+        }
+
+//        private void setNullActivity(){
+//            this.mainActivity = null;
+//        }
+
+        /**
+         * バックグランドで行う処理
+         */
+        @Override
+        protected Boolean doInBackground(Void... value) {
+            try {
+                if(!isCancelled()){
+                    publishProgress(this.mainActivity.kaitou);
+                    Thread.sleep(2000);
+                    return true;
+                }else{
+                    cancel(true);
+                    return false;
+                }
+            } catch (Exception e) {
+                exception = e;
+                cancel(true);
+                return false;
+            }
+        }
+
+        // doInBackground中にUI更新
+        @Override
+        protected void onProgressUpdate(Boolean... progress) {
+            for (Boolean bool : progress) {
+                try{
+                    if(bool){
+                        this.mainActivity.imageView.setImageResource(R.mipmap.maru2);
+                        this.mainActivity.maru.start();
+                    }else{
+                        this.mainActivity.imageView.setImageResource(R.mipmap.batu2);
+                        this.mainActivity.batu.start();
+                    }
+                }catch (Exception e){
+                    cancel(true);
+                }
+            }
+        }
+
+        private boolean shouldReject() {
+            MainActivity activity = this.mainActivity;
+
+            if (activity != null) {
+                return activity.isFinishing() || !activity.hasWindowFocus();
+            } else {
+                return true;
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            if (!shouldReject()) {
+                if (exception != null) {
+                    Log.e("exception", "exception : " + exception);
+                }
+            }
+            exception = null;
+        }
+
+        /**
+         * バックグランド処理が完了し、UIスレッドに反映する
+         */
+        @Override
+        protected void onPostExecute(Boolean result) {
+            cancel(true);
+            this.mainActivity.updateQuiz();
+        }
+    }
 }
+
+// maru batu 中にバックキーがやばい
 
--- app/src/main/java/com/example/nttr/quiz/ResultActivity.java
+++ app/src/main/java/com/example/nttr/quiz/ResultActivity.java
@@ -1,13 +1,40 @@
 package com.example.nttr.quiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ResultActivity extends Activity {
+public class ResultActivity extends Activity implements View.OnClickListener{
+
+    int score = 0;
+
+    TextView textView;
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        score = intent.getIntExtra("score", 0);
+
+        textView = (TextView) findViewById(R.id.scoreText);
+        textView.setText("score : " + score);
+
+        button = (Button) findViewById(R.id.retryButton);
     }
+
+    // ⑦ setOnClickListerを呼び出したViewをクリックした時に呼び出されるメソッド
+    @Override
+    public void onClick(View view) {
+        finish();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
